f34312fe7e4ee8a3b24efc5d5b56275a6ff34a32
Jan Návrat <jan.navrat@ceskatelevize.cz>
2026-03-05 18:58:52 +0100

    ADD EXPORT BUTTON

--- frontend/src/routes/quick-settings.tsx
+++ frontend/src/routes/quick-settings.tsx
@@ -4,9 +4,11 @@ import { InlineLink } from "components/interactive/inline-link";
 import { useLanguage } from "components/localisation";
 import { PageHeader } from "components/page-header";
 import { TH2, TText } from "components/themed";
+import { saveAs } from "file-saver";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { StyleSheet, View } from "react-native";
+import { useSongList } from "store/store";
 
 export default function QuickSettings() {
   const { t } = useTranslation();
@@ -50,7 +52,37 @@ export default function QuickSettings() {
       <Titled title={t("quick-settings.More options")}>
         <InlineLink to="/about">{t("Settings and about")}</InlineLink>
       </Titled>
+      <ExportSongs />
     </div>
+  );
+}
+
+function ExportSongs() {
+  const { t } = useTranslation();
+  const { songs } = useSongList();
+  return (
+    <Titled title={t("Export")}>
+      <button
+        style={{
+          border: "1px solid currentColor",
+          borderRadius: 4,
+          paddingInline: 16,
+          paddingBlock: 8,
+          background: "transparent",
+          color: "inherit",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          const data = songs.map((s) => s.item);
+          const blob = new Blob([JSON.stringify(data, null, 2)], {
+            type: "application/json;charset=utf-8",
+          });
+          saveAs(blob, "songbook-export.json");
+        }}
+      >
+        {t("Export all songs")}
+      </button>
+    </Titled>
   );
 }
 
